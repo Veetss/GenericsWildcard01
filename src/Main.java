e1129b82e95f1c501bf47fff6154e9e64d6f24e3
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:41:54 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Double> individualMarksListLisa = new ArrayList<>();
+        List<Integer> projectMarksListJeremy = new ArrayList<>();
+
+        individualMarksListLisa.add(0, 9.5);
+        individualMarksListLisa.add(1, 7.8);
+
+        projectMarksListJeremy.add(0, 5);
+        projectMarksListJeremy.add(1, 2);
+
+        double LisaAvg = Student.getAverageMark(individualMarksListLisa);
+        double JeremyAvg = Student.getAverageMark(projectMarksListJeremy);
+
+        List<Student> listOfStudents = new ArrayList<>();
+
+        listOfStudents.add(new Student("Lisa", "Stuart", LisaAvg));
+        listOfStudents.add(new Student("Jeremy", "Green", JeremyAvg));
+
+        System.out.println(listOfStudents.get(0).toString());
+        System.out.println(listOfStudents.get(1).toString());
     }
 }
